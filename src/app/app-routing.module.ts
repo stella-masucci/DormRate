2dc7403d5b320d9c079e03a8cb481df4ecd5c0f6
stella.masucci <stella.masucci@gmail.com>
2021-04-27 22:27:05 -0400

    generated about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -45,6 +45,10 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
   }
 ];
 @NgModule({
