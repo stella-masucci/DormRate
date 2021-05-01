5aa507deaafb29c554853e0547352266fb908eba
The Nhan LUONG <thenhan.luong@amadeus.com>
2021-05-01 15:32:15 +0200

    Add Review, Profile, Logout are only displayed if user logged in

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -7,10 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
-
+  userLoggedIn : boolean = false;
+  
   constructor(
-    public afAuth: AngularFireAuth
-  ) { }
+    public afAuth: AngularFireAuth,
+  ) {
+    this.afAuth.authState.subscribe(auth => {
+      if (auth) {
+        this.userLoggedIn = true;
+      }
+    });
+  }
 
   logout() {
     this.afAuth.signOut();
